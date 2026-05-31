c81a688bb103a45633ef4ec864143136b12e6355
Jefferson19620 <jeffersondossantoslimasousa@gmail.com>
2026-05-30 23:11:48 -0300

    Estrutura do Router v1.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App.jsx";
+import ErrorPage from "./ErroPage.jsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+
+    children: [
+      {
+        path: "/",
+        element: <home />,
+      },
+
+      {
+        path: "/cadastrar",
+        element: <Cadastrar />,
+      },
+
+      {
+        path: "/login",
+        element: <login />,
+      },
+
+      {
+        path: "/feed",
+        element: <feed />,
+      },
+      {
+        path: "/Anonimo",
+        element: <Anonimo />,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
